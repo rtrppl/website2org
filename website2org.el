ed08c906706565cde00bfe843f310d0c768289f4
rtrppl <63909884+rtrppl@users.noreply.github.com>
2024-10-22 16:09:48 +0200

    address read-only issue
    Now website2org should also work when invoked on URLs in read-only buffers.

--- website2org.el
+++ website2org.el
@@ -65,7 +65,8 @@ website2org-url-to-org. Results will be presented in a buffer."
     (website2org-to-buffer url)))
 
 (defun website2org-url-to-org (url)
- "Creates an Orgmode document from a URL."
+ "Creates an Orgmode document from an URL."
+ (with-temp-buffer
   (website2org-create-local-cache-file url)
   (let* ((content (website2org-load-file website2org-cache-filename))
 	 (title (website2org-process-html content "title"))
@@ -81,24 +82,24 @@ website2org-url-to-org. Results will be presented in a buffer."
       (insert (concat website2org-additional-meta "\n")))
     (insert (concat "#+roam_key: " url "\n\n"))
     (insert org-content)
-    (goto-char (point-min))))
+    (goto-char (point-min)))))
 
 (defun website2org-to-buffer (URL)
-  "Returns the text from a html-file."
-  (interactive)
-  (website2org-create-local-cache-file URL)
-  (let* ((content (website2org-load-file website2org-cache-filename))
-	 (title (website2org-process-html content "title"))
-	 (org-content (website2org-process-html content "content"))
-	 (final))
-    (website2org-delete-local-cache-file)
+  "Creates an Orgmode buffer from an URL."
+  (with-temp-buffer
+    (website2org-create-local-cache-file URL)
+    (let* ((content (website2org-load-file website2org-cache-filename))
+	   (title (website2org-process-html content "title"))
+	   (org-content (website2org-process-html content "content"))
+	   (final))
+      (website2org-delete-local-cache-file)
     (setq final (concat "#+roam_key: " URL "\n\n" org-content))
     (setq final (concat "#+title: " title "\n" final))
     (with-current-buffer (get-buffer-create "website2org")
       (erase-buffer)
       (switch-to-buffer "website2org")
       (insert final)))
-  (website2org-prepare-findings-buffer "website2org"))
+  (website2org-prepare-findings-buffer "website2org")))
 
 (defun website2org-prepare-findings-buffer (buffer)
  "Preparing the orgrr findings buffer."
